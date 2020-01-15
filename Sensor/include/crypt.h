fc1c3b69ca4e2343745547cf0b2aa34f061f4941
Max Hughson <max.ak.hughson@gmail.com>
2020-01-15 11:10:26 -0600

    Started writing code to format strings nicely for mqtt

---
+++ Sensor/include/crypt.h
@@ -0,0 +1,21 @@
+#ifndef MQTT_CRYPT_H
+#define MQTT_CRYPT_H
+#include <string>
+#include <vector>
+
+
+
+std::vector<std::vector<unsigned char> > make_AES_blocks(
+	const std::string& source
+);
+
+std::string encrypt_string_AES(
+	std::string keygen,
+	std::string input
+);
+
+std::string decrypt_string_AES(
+	std::string keygen,
+	std::string input
+);
+#endif
