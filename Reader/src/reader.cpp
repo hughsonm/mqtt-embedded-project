8b6130210ad5f0cd86232dde4f6059d22c8aeb8e
Max Hughson <max.ak.hughson@gmail.com>
2020-01-16 13:03:29 -0600

    Got the topics encoded. MQTT didn't like having tabs and no slashes in the topic, so I prefix the topic with some plain string, then smoosh the encoded topic suffix and slap it on the end.
    Still having some difficulty decoding the payload at the reader.

--- Reader/src/reader.cpp
+++ Reader/src/reader.cpp
@@ -5,24 +5,47 @@
 #include <mqtt/callback.h>
 #include <wiringPi.h>
 #include <chrono>
+#include <fstream>
+#include "crypt.h"
 
 const std::string broker_address{"130.179.196.54:1883"};
+const std::string keygen_filename{"~/aes_key_gen.txt"};
+const std::string raw_topic{"maxtopic/periodic/collatz"};
+
 
 class callback : public virtual mqtt::callback{
+	std::string _kg;
 	void message_arrived(mqtt::const_message_ptr msg) override {
 		std::cout << "Message arrived" << std::endl;
-		std::cout << "\ttopic:\t'" << msg->get_topic() << "'" << std::endl;
-		std::cout << "\tpayload:\t'" << msg->to_string() << "'\n" << std::endl;
+		std::cout << "\ttopic:\t'" << msg->get_topic() << "'\n";
+		const std::string enc_payload{msg->to_string()};
+		std::cout << "\tpayload:\t'" << enc_payload << "'\n";
+		const std::string dec_payload{
+			decrypt_string_AES(_kg,enc_payload)
+		};
+		std::cout << "\tmessage:\t" << dec_payload << "\n";
 	};
+public:
+	callback(std::string kg){_kg = kg;};
 };
 
+
 int main(int argc,char** argv){
+	
+	std::ifstream reader;
+	reader.open(keygen_filename,std::ifstream::in);
+	std::string keygen;
+	reader >> keygen;
+	reader.close();
+	
+	const std::string enc_topic{
+		encrypt_topic_AES(keygen,"hughson",raw_topic)
+	};
+	
 	std::cout << "Running..." << std::endl;
-
-
 	std::cout << "Constructing client..." << std::endl;
-	mqtt::async_client cl(broker_address,"hughsonPiReader");
-	callback cb;
+	mqtt::async_client cl(broker_address,"hpReader");
+	callback cb(keygen);
 	cl.set_callback(cb);
 	
 	std::cout << "Connecting client..." << std::endl;
@@ -31,7 +54,8 @@ int main(int argc,char** argv){
 	std::cout << "Connected!" << std::endl;
 
 	//mqtt::topic top(cl,"maxtopic/periodic/collatz");
-	cl.subscribe("maxtopic/periodic/collatz",1);
+	std::cout << "Listening to topic " << enc_topic << "\n";
+	cl.subscribe(enc_topic,1);
 		
 	while (std::tolower(std::cin.get()) != 'q');
 	
--- Sensor/src/sensor.cpp
+++ Sensor/src/sensor.cpp
@@ -14,6 +14,28 @@ const std::string broker_address{"130.179.196.54:1883"};
 const std::string keygen_filename{"~/aes_key_gen.txt"};
 const std::string raw_topic{"maxtopic/periodic/collatz"};
 
+class Sensor{
+	double ym2{0.0};
+	double ym1{0.0};
+	double y{0.0};
+	double xm2{1.0};
+	int count{0};
+public:
+	double read_normalized_value();
+};
+
+double Sensor::read_normalized_value(){
+	y = 0.9*(1.905*ym1-ym2+xm2);
+	ym2 = ym1;
+	ym1 = y;
+	xm2 = 0.0;
+	if(100<++count){
+		xm2 = 1.0;
+		count = 0;
+	}
+	return(y);
+}
+
 
 int main(int argc,char** argv){
 	
@@ -23,14 +45,16 @@ int main(int argc,char** argv){
 	reader >> keygen;
 	reader.close();
 	
-	const std::string enc_topic{encrypt_string_AES(keygen,raw_topic)};
+	const std::string enc_topic{
+		encrypt_topic_AES(keygen,"hughson",raw_topic)
+	};
 	
 	std::cout << enc_topic <<"\n";
 	
 	std::cout << "Running..." << std::endl;
 	
 	std::cout << "Constructing client..." << std::endl;
-	mqtt::async_client cl(broker_address,"hughsonPiSensor");
+	mqtt::async_client cl(broker_address,"hpSensor");
 	
 	std::cout << "Client is currently ";
 	if(not(cl.is_connected())) std::cout << "not ";
@@ -39,15 +63,13 @@ int main(int argc,char** argv){
 	std::cout << "Connecting client" << std::endl;
 	cl.connect();
 	
-	while(not(cl.is_connected())){
-		std::cout << ".";
-	}
+	while(not(cl.is_connected()));
 	std::cout << std::endl;
 	
 	// Every T seconds, publish a message to maxtopic/periodic/collatz
 	mqtt::topic top(cl,enc_topic);
-	int nn{2};
-	int ii{nn};
+	std::cout << "Broadcasting on topic " << enc_topic << "\n";
+	Sensor meter;
 	auto t_i{std::chrono::steady_clock::now()};
 	while(true){
 		const auto t_f{std::chrono::steady_clock::now()};
@@ -55,26 +77,19 @@ int main(int argc,char** argv){
 			std::chrono::duration_cast<std::chrono::seconds>(t_f-t_i)
 			};
 		if(5 <=  dt.count()){
-			t_i=t_f;
-			if(ii == 1){
-				nn++;
-				ii=nn;
-			} else {
-				if(ii%2){
-					ii = 3*ii+1;
-				} else{
-					ii /= 2;
-				}
-			}
-			const std::string enc_ii{
-				encrypt_string_AES(
-					keygen,
-					std::to_string(ii)
-				)
+			t_i = t_f;
+			const double reading{meter.read_normalized_value()};
+			const std::string enc_reading{
+				encrypt_string_AES(keygen,std::to_string(reading))
 			};
-			std::cout << ii << "\t" << enc_ii << "\n";
+			std::cout << reading << "\n";
+			std::cout << enc_reading << "\n";
+			std::string dec_reading{
+				decrypt_string_AES(keygen,enc_reading)
+			};
+			std::cout << dec_reading << "\n";
 			top.publish(
-				std::move(std::to_string(ii))
+				std::move(enc_reading)
 			);
 		}
 	}
